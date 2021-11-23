1aa7adaa9aebf7393e5835e353286905bce30571
JailsonSantos <jailson.ads007@gmail.com>
2021-11-23 17:04:17 -0300

    Configuração da Base da API

---
+++ api/app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $hidden = ['password'];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/ping', function () {
+    return ['pong' => true];
+});
+
+// Auths
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+// Users
+Route::get('/user', [UserController::class, 'read']);
+Route::put('/user', [UserController::class, 'update']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('/user/favorite', [UserController::class, 'addFavorite']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+// Barbers
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barber/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment']);
+
+// Searchs
+Route::get('/search', [BarberController::class, 'search']);
